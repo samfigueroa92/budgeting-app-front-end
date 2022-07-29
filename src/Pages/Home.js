e4575bf3179c866ebf9524683e8428ebb26af265
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-07-29 13:04:57 -0400

    remove link

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,6 @@
 import { Container } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
